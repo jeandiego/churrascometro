d2fed4b93750e97bbcdd41bab16eb6f84fc6a4cf
Jean Diego <jeean.james@gmail.com>
2021-03-09 17:13:59 -0300

    ✨ New scripts added

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,24 @@
 // Carne - 400 g por pessoa, se o churrasco durar mais de 6 horas 650g por pessoas.
-
 // Cerveja - 1200 ml por pessoa, 
 // se o churrasco durar mais de 6 horas 2000 ml por pessoal.
-
 // Refrigerante/agua - 1000 ml por pessoa, 
 // se o churrasco durar mais de 6 horas 1500 ml por pessoal.
-
 // Criança equivale a 1/2 pessoa.
+// Cada 6kg de carne utiliza 5kg de carvao = 1,2kg carvao por kg de carne
 
 
 let inputAdults = document.getElementById("adults");
-
-
 let inputKids = document.getElementById("kids");
-
-
 let inputTime = document.getElementById("time");
-
-
 let inputFields = document.getElementById("calculatorContainer")
-let results = document.getElementById("calculatorResults")
+let results = document.getElementById("calculatorResults");
 
+let meat = document.getElementById("result-meat");
+let beer = document.getElementById("result-beer");
+let drinks = document.getElementById("result-drinks");
+let coal = document.getElementById("result-coal");
+
+// let coalUsage = 1.2;
 
 function calculate() {
   console.log("Calculando...")
@@ -32,11 +30,15 @@ function calculate() {
   let totalMeat = meatPerPerson(time) * adults + (meatPerPerson(time) / 2 * kids);
   let totalBeer = beerPerPerson(time) * adults;
   let totalDrink = drinkPerPerson(time) * adults + (drinkPerPerson(time) / 2 * kids);
+  let totalCoal = (totalMeat * 5) / 6;
   
   inputFields.classList.replace("show", "hide");
   results.classList.replace("hide","show");
 
-  // console.log(totalMeat, totalBeer, totalDrink);
+  meat.innerHTML = `${Math.ceil(totalMeat / 1000 )} kg`;
+  beer.innerHTML = `${Math.ceil(totalBeer / 350)} latas`;
+  drinks.innerHTML = `${Math.ceil(totalDrink / 1000)} litros`;
+  coal.innerHTML = `${Math.ceil(totalCoal / 5000)} sacos`;
 }
 
 function meatPerPerson(time) {
@@ -68,4 +70,11 @@ function reCalculate() {
   results.classList.replace("show","hide");
 
   console.log("Recalculando")
+}
+
+
+function cleanValue() {
+  inputAdults.value = "";
+  inputKids.value = "";
+  inputTime.value = "";
 }
